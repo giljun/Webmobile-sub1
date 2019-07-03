8fc8ad4fef08ed5edd4fb0e0dbcfb68432e9ce0c
박길준 <adda281@naver.com>
2019-07-03 17:04:31 +0900

    modify markdown editor

--- webmobile/src/views/EditorPage.vue
+++ webmobile/src/views/EditorPage.vue
@@ -1,9 +1,19 @@
 <template>
   <v-container fluid :grid-list-md="!$vuetify.breakpoint.xs">
     <v-layout wrap row>
+      <v-text-field
+        label="Title"
+        v-model="title"
+        prepend-icon="folder"
+      ></v-text-field>
+    </v-layout>
+    <v-layout wrap row>
       <v-flex>
-        <editor v-model="editorText"/>
+        <editor v-model="editorText" />
       </v-flex>
+    </v-layout>
+    <v-layout wrap row>
+      <v-btn flat class="success mx-0 mt-3">Add Portfolio</v-btn>
     </v-layout>
   </v-container>
 </template>
