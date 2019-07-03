3d626eec9e3eff23f583b17c5084a580cd1dda4c
박길준 <adda281@naver.com>
2019-07-03 14:23:55 +0900

    Req. 1-15 포트폴리오, 포스트 리스트에서 타이틀은 한 줄, 설명은 3, 4줄 표시 구현

--- webmobile/src/components/Portfolio.vue
+++ webmobile/src/components/Portfolio.vue
@@ -1,11 +1,12 @@
 <template>
   <v-card>
-    <v-img :src="imgSrc" height="200px">
-    </v-img>
+    <v-img :src="imgSrc" height="200px"> </v-img>
     <v-card-title primary-title>
       <div>
-        <div class="headline">{{title}}</div>
-        <span class="grey--text">{{body}}</span>
+        <div class="headline Portfolio_Title">
+          {{ title }}
+        </div>
+        <span class="grey--text Portfolio_Body">{{ body }}</span>
       </div>
     </v-card-title>
   </v-card>
@@ -13,18 +14,37 @@
 
 <script>
 export default {
-	name: 'Portfolio',
-	props: {
-		date: {type: String},
-		title: {type: String},
-		body: {type: String},
-		imgSrc: {type: String},
-	},
-	data() {
-		return {
-			//
-		}
-	}
-}
+  name: "Portfolio",
+  props: {
+    date: { type: String },
+    title: { type: String },
+    body: { type: String },
+    imgSrc: { type: String }
+  },
+  data() {
+    return {
+      //
+    };
+  }
+};
 </script>
 
+<style>
+.Portfolio_Title {
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  width: 85%;
+}
+.Portfolio_Body {
+  overflow: hidden;
+  text-overflow: ellipsis;
+  display: -webkit-box;
+  -webkit-line-clamp: 3; /* 라인수 */
+  -webkit-box-orient: vertical;
+  word-wrap: break-word;
+  line-height: 1.2em;
+  height: 3.6em;
+  width: 95%;
+}
+</style>
--- webmobile/src/components/Post.vue
+++ webmobile/src/components/Post.vue
@@ -1,37 +1,55 @@
 <template>
   <v-layout py-4 h-100>
     <v-flex row>
-      <div class="caption">{{formatedDate}}</div>
-      <h2 class="color-333 headline font-weight-light">{{title}}</h2>
-      <p class="mb-1 color-666 font-weight-light subheading">{{body}}</p>
+      <div class="caption">{{ formatedDate }}</div>
+      <h2 class="color-333 headline font-weight-light">{{ title }}</h2>
+      <p class="mb-1 color-666 font-weight-light subheading Post_Body">
+        {{ body }}
+      </p>
     </v-flex>
   </v-layout>
 </template>
 
 <script>
 export default {
-	name: 'Post',
-	props: {
-		date: {type: Date},
-		title: {type: String},
-		body: {type: String}
-	},
+  name: "Post",
+  props: {
+    date: { type: Date },
+    title: { type: String },
+    body: { type: String }
+  },
   computed: {
-		formatedDate() {
-			return `${this.date.getFullYear()}년 ${this.date.getMonth()}월 ${this.date.getDate()}일`
+    formatedDate() {
+      return `${this.date.getFullYear()}년 ${this.date.getMonth()}월 ${this.date.getDate()}일`;
     }
   }
-}
+};
 </script>
 <style>
-  .color-666 {
-    color: #666;
-  }
-  .color-333 {
-    color: #333;
-  }
-  .h-100 {
-    height: 100%;
-  }
+.color-666 {
+  color: #666;
+}
+.color-333 {
+  color: #333;
+}
+.h-100 {
+  height: 100%;
+}
+.Post_Body {
+  overflow: hidden;
+  text-overflow: ellipsis;
+  display: -webkit-box;
+  -webkit-line-clamp: 3; /* 라인수 */
+  -webkit-box-orient: vertical;
+  word-wrap: break-word;
+  line-height: 1.2em;
+  height: 3.6em; /* line-height 가 1.2em 이고 3라인을 자르기 때문에 height는 1.2em * 3 = 3.6em */
+  width: 95%;
+}
+.Post_Title {
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  width: 85%;
+}
 </style>
-
